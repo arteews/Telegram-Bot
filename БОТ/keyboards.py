189040c88ee7db11fc3082c2a40b731c466c9a01
Романенко Артём Сергеевич <80912870+arteews@users.noreply.github.com>
2024-12-11 16:36:44 +0300

    Delete БОТ directory
    Удаление недоделанного бота

--- БОТ/keyboards.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
-
-
-def get_main_menu():
-    keyboard = [
-        [KeyboardButton(text="📊 Профиль"), KeyboardButton(text="🛠️ Техподдержка")],
-        [KeyboardButton(text="✍️ Отзывы и предложения"), KeyboardButton(text="👉 Продолжить")]
-    ]
-    return ReplyKeyboardMarkup(keyboard=keyboard, resize_keyboard=True)
--- БОТ/status.py
+++
@@ -1,5 +0,0 @@
-from aiogram.fsm.state import StatesGroup, State
-
-
-class ConfigState(StatesGroup):
-    waiting_for_age = State()
